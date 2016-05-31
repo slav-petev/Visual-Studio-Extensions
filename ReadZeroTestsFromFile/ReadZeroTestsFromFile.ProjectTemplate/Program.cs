066ff899fc0d95fc7836dc73757e0b2da11e06f6
Slav Petkov <Slav Petkov@PC-1138>
2016-05-31 15:56:39 +0300

    Set the correct output type for the generated project

---
+++ ReadZeroTestsFromFile/ReadZeroTestsFromFile.ProjectTemplate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Not to be submitted to Judge
+        SetInputStream();
+
+        string currentZerotestInput = Console.ReadLine();
+        while (currentZerotestInput != null)
+        {
+            currentZerotestInput = Console.ReadLine();
+        }
+    }
+
+    [Conditional("DEBUG")]
+    private static void SetInputStream()
+    {
+        Console.SetIn(new StreamReader(@"../../Input/Input.txt"));
+    }
+}
