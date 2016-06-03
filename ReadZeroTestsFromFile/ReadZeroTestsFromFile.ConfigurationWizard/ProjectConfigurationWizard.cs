94841d7cfe489c13fb0112d1e644ec839fbd4eb5
Slav Petkov <Slav Petkov@PC-1138>
2016-06-03 12:23:09 +0300

    Successfully added custom wizard to project

--- ReadZeroTestsFromFile/ReadZeroTestsFromFile.ConfigurationWizard/ProjectConfigurationWizard.cs
+++ ReadZeroTestsFromFile/ReadZeroTestsFromFile.ConfigurationWizard/ProjectConfigurationWizard.cs
@@ -1,40 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 
-namespace ReadZeroTestsFromFile.Extension
+namespace ReadZeroTestsFromFile.ConfigurationWizard
 {
-    public class InitializationWizard : IWizard
+    public class ProjectConfigurationWizard : IWizard
     {
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
-            
+            throw new Exception("My Exception");
         }
 
         public void ProjectFinishedGenerating(Project project)
         {
-            
+            throw new NotImplementedException();
         }
 
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
-            
+            throw new NotImplementedException();
         }
 
         public bool ShouldAddProjectItem(string filePath)
         {
-            return true;
+            throw new NotImplementedException();
         }
 
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
-            
+            throw new NotImplementedException();
         }
 
         public void RunFinished()
         {
-        
+            throw new NotImplementedException();
         }
     }
 }
